371ab96f5e793f6da7400e5168c2db254b1cc772
Iceberry-qdd <qddwork@outlook.com>
2021-07-18 11:53:10 +0800

    first full version

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    const windowHeight = $(window).height()
+    let lastPageIndex = -1
+    let lastDelta = -1
+    $(".full-screen-container").height(windowHeight)
+})
